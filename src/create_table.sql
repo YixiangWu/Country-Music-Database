d0d257acd1a51a7228dfedbc9b9a4e70b00cb924
Yixiang <gavinwu964@gmail.com>
2023-12-08 21:27:45 -0500

    Add artist rankings

--- src/create_table.sql
+++ src/create_table.sql
@@ -1,3 +1,15 @@
+CREATE TABLE artist_rankings (
+    year INTEGER,
+    rank INTEGER,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    stage_name VARCHAR(50),
+    best_song_for_the_year VARCHAR(100)
+);
+
+COPY artist_rankings FROM '/path/to/artist_rankings.csv' WITH (FORMAT CSV, HEADER);
+
+
 CREATE TABLE demographics (
     first_name VARCHAR(50),
     middle_name VARCHAR(50),
